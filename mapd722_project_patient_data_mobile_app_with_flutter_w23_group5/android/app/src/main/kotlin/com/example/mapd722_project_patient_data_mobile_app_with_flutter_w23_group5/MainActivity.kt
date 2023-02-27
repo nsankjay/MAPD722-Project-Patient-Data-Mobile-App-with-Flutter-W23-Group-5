3f37c1cd1c10ab99aae1cd0184fa8c11e801a0c5
nsankjay <113073827+nsankjay@users.noreply.github.com>
2023-02-27 12:23:23 -0500

    Created project and updated .gitignore file

---
+++ mapd722_project_patient_data_mobile_app_with_flutter_w23_group5/android/app/src/main/kotlin/com/example/mapd722_project_patient_data_mobile_app_with_flutter_w23_group5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mapd722_project_patient_data_mobile_app_with_flutter_w23_group5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
